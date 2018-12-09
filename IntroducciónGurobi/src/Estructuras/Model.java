29650e960b59131e7848b2a7930288ec57b7fd29
Nicolás Vásquez Murcia <n.vasquez10@uniandes.edu.co>
2018-12-08 23:32:35 -0500

    Cambios en model

--- IntroducciónGurobi/src/Estructuras/Model.java
+++ IntroducciónGurobi/src/Estructuras/Model.java
@@ -1,6 +1,7 @@
 package Estructuras;
 
 import java.util.ArrayList;
+import Lectura.*;
 
 import gurobi.*;
 
@@ -14,71 +15,96 @@ public class Model {
 	private static int numNodosTotal;
 	
 	private static int[][] matrizAdyacencia;
-	private static int numEsquinas;
+	private static double[][] matrizCostos;
 	
 	public static void main(String[] args){
 		
 		//TODO: Cargar el archivo de datos para armar el grafo
-		Reader leer = new Reader("src/RMC.txt");
+		Reader grafo = new Reader("./data/Temp_GCUT16.txt");
 		
-		Grafo migrafo;
-		migrafo=leer.getG();
+		matrizAdyacencia = grafo.getMatrizAdyacencia();
+		matrizCostos = grafo.getMatrizCostos();
+		//Existen 2 nodos dummies
+		numNodosTotal = grafo.getNumNodosAire()+grafo.getNumNodosCorte()+2;
 		
+		numNodosCorte= grafo.getNumNodosCorte();
 		//Crear entorno para gurobi
 		GRBEnv env;
-		
 		
 		try{
 			
 			env = new GRBEnv(null);
 			GRBModel model = new GRBModel(env);
-			
 			//Crear las variables binarias 
-			
-			for(int i=0;i<migrafo.getArcos().size();i++){
-				model.addVar(0,1,migrafo.getArcos().get(i).getCost(),GRB.BINARY,"x("+migrafo.getArcos().get(i).getTail()+","+migrafo.getArcos().get(i).getHead()+")");
+			int arco=0;
+			for(int i=0;i<matrizAdyacencia.length;i++)
+			{
+				for(int j=0;j<matrizAdyacencia.length;j++)
+				{
+					arco = matrizAdyacencia[i][j];
+					if(arco>0)
+					{
+						//Agrega variable que representa ir de nodo i a j y devuelta
+						model.addVar(0,1,matrizCostos[i][j],GRB.BINARY,"x("+i+","+j+")");
+						if(i==94 && j == 1)
+						{
+							System.out.println("si esta");
+						}
+					}
+					if(i==94 && j == 1)
+					{
+						System.out.println("si esta");
+					}
+				}
 			}
 			model.update();
-			
 			
 			//Crear las restricciones de balance
 			GRBLinExpr balance;
 			
-			//TODO: Estas restricciones de balance tienen que ser para todos los nodos menos el nodo inicial y final
-			for(int i=1;i<migrafo.getNodos().size();i++){	
+			//Estas restricciones de balance tienen que ser para todos los nodos menos el nodo inicial y final
+			for(int i=0;i<numNodosTotal;i++){	
 				balance = new GRBLinExpr();
-				for(int j=0;j<migrafo.getArcos().size();j++){
-					if(migrafo.getArcos().get(j).getTail()==migrafo.getNodos().get(i)){
-						balance.addTerm(1, model.getVarByName("x("+migrafo.getArcos().get(j).getTail()+","+migrafo.getArcos().get(j).getHead()+")"));
+				for(int j=0;j<numNodosTotal;j++){
+					//La sumatoria de los arcos disidentes
+					if(matrizAdyacencia[i][j]==1){
+						balance.addTerm(1, model.getVarByName("x("+i+","+j+")"));
 					}
-					if(migrafo.getArcos().get(j).getHead()==migrafo.getNodos().get(i)){
-						balance.addTerm(-1, model.getVarByName("x("+migrafo.getArcos().get(j).getTail()+","+migrafo.getArcos().get(j).getHead()+")"));
+					//Menos la sumatoria de los arcos incidentes
+					if(matrizAdyacencia[j][i]==1){
+						balance.addTerm(-1, model.getVarByName("x("+j+","+i+")"));
 					}
 				}
 				
 				if(i!=0 && i<numNodosTotal-1){
-					model.addConstr(balance,GRB.EQUAL,0,"Balance nodo"+i);
+					model.addConstr(balance,GRB.EQUAL,0,"Balance nodo "+i);
 				} else if(i==0){
-					model.addConstr(balance, GRB.EQUAL, 1,"Balance_"+i);
+					model.addConstr(balance, GRB.EQUAL, 1,"Balance nodo inicial");
 				}
 				else {
-					model.addConstr(balance, GRB.EQUAL, -1,"Balance_"+i);
+					model.addConstr(balance, GRB.EQUAL, -1,"Balance nodo final");
 				}
 			}
+			model.update();
 			
 			//Crear restriccion para cortar una sola vez por cada arco
 			GRBLinExpr corteU;
 			for(int i =1; i<numNodosCorte;i++)
 			{
 				for(int j =i+1; j<numNodosCorte;j++){
-					corteU = new GRBLinExpr();
-					corteU.addTerm(1, model.getVarByName("x("+migrafo.getArcos().get(j).getTail()+","+migrafo.getArcos().get(j).getHead()+")"));
-					corteU.addTerm(1, model.getVarByName("x("+migrafo.getArcos().get(j).getHead()+","+migrafo.getArcos().get(j).getTail()+")"));
-					model.addConstr(corteU, GRB.EQUAL, 1, "Corte Unico"+i+j);
+					if(matrizAdyacencia[i][j]==1)
+					{
+						corteU = new GRBLinExpr();
+						corteU.addTerm(1, model.getVarByName("x("+i+","+j+")"));
+						corteU.addTerm(1, model.getVarByName("x("+j+","+i+")"));
+						System.out.println(model.getVarByName("x("+i+","+j+")"));
+						System.out.println(model.getVarByName("x("+j+","+i+")"));
+						System.out.println(j+","+i);
+						model.addConstr(corteU, GRB.EQUAL, 1, "Corte Unico entre "+i+" y "+j);
+					}
 				}
 			}
 			
-						
 			/*
 			 * 1: Minimización
 			 * -1: Maximización
